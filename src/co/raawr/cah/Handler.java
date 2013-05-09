484fe22e721ba967be8a6545863fb82ce9d9794c
Rurigok <>
2013-05-08 18:15:15 -0600

    Committing some random change

--- src/co/raawr/cah/Handler.java
+++ src/co/raawr/cah/Handler.java
@@ -18,55 +18,62 @@ public class Handler {
 
         String command = parse[0].toLowerCase();
 
-        if (command.startsWith(".")) {
-            if (command.equals(".cah")) {
-                if (parse.length > 1) {
-                    if (isInteger(parse[1])) {
-                        CAH.prepGame(Integer.parseInt(parse[1]), CAH.createPlayer(nick));
-                    }
-                }
-            } else if (channel.equals("#cah")) {
-                // It's a command - handle it
-                switch (command) {
-                    case ".join":
-                        CAH.addPlayer(CAH.createPlayer(nick));
-                        break;
-                    case ".leave":
-                    case ".quit":
-                        CAH.removePlayer(CAH.lookupPlayer(nick));
-                        break;
-                    case ".start":
-                        CAH.begin(CAH.lookupPlayer(nick));
-                        break;
-                    case ".end":
-                        CAH.endGame(CAH.lookupPlayer(nick));
-                        break;
-                    case ".score":
-                        CAH.getScore(CAH.lookupPlayer(nick));
-                        break;
-                    case ".stop":
-                        CAH.endGame(CAH.lookupPlayer(nick));
-                        break;
-                    case ".sudo":
-                        //sudo(nick, parse[1], channel, parse[2]);
-                        break;
-                    case ".scan":
-                    case ".rescan":
-                        //CAH.rescanCards();
-                        break;
-                    case ".remove":
-                    case ".kick":
-                        if (parse.length == 2) {
-                            CAH.kickPlayer(CAH.lookupPlayer(nick), CAH.lookupPlayer(parse[1]));
-                        }
-                        break;
-                    default:
-                        break;
+        if (parse.length == 1 && isInteger(command)) {
+            CAH.czarPickCard(CAH.lookupPlayer(nick), Integer.parseInt(command));
+            return;
+        }
+
+        if (!command.startsWith(".") && !command.startsWith("!")) {
+            return;
+        }
+
+        command = command.replaceFirst("!", "");
+        command = command.replaceFirst("\\.", "");
+
+        if (command.equals("cah")) {
+            if (parse.length == 2) {
+                if (isInteger(parse[1])) {
+                    CAH.prepGame(Integer.parseInt(parse[1]), CAH.createPlayer(nick));
                 }
             }
-        } else if (isInteger(command)) {
-            // It's the czar picking a card
-            CAH.czarPickCard(CAH.lookupPlayer(nick), Integer.parseInt(command));
+        } else if (channel.equals("#cah")) {
+            // It's a command - handle it
+            switch (command) {
+                case "join":
+                    CAH.addPlayer(CAH.createPlayer(nick));
+                    break;
+                case "leave":
+                case "quit":
+                    CAH.removePlayer(CAH.lookupPlayer(nick));
+                    break;
+                case "start":
+                    CAH.begin(CAH.lookupPlayer(nick));
+                    break;
+                case "end":
+                    CAH.endGame(CAH.lookupPlayer(nick));
+                    break;
+                case "score":
+                    CAH.getScore(CAH.lookupPlayer(nick));
+                    break;
+                case "stop":
+                    CAH.endGame(CAH.lookupPlayer(nick));
+                    break;
+                case "sudo":
+                    //sudo(nick, parse[1], channel, parse[2]);
+                    break;
+                case "scan":
+                case "rescan":
+                    //CAH.rescanCards();
+                    break;
+                case "remove":
+                case "kick":
+                    if (parse.length == 2) {
+                        CAH.kickPlayer(CAH.lookupPlayer(nick), CAH.lookupPlayer(parse[1]));
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
